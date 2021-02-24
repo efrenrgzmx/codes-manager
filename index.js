0617b1cb5d8410a2421cdd80e1b38b26362d62d2
Efren Rodriguez <efren.rodriguez@protexa.com.mx>
2021-02-24 12:32:50 -0600

    [bugfix] procfile

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
  * **/
 
 //COMUNICACION WEBSOCKET
-var express = require('express');
-var app = express();
-const http = require('http').Server(app);
+const express = require('express');
+const path = require('path');
 
-app.use(express.static('public'));
+const app = express();
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static('./public'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/public/index.html'));
 });
 
-var port_number = http.listen(process.env.PORT || 3000);
-app.listen(port_number);
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
 
