8d7b371540118d054873baf7d9376b121eacb3fa
Efren Rodriguez <efren.rodriguez@protexa.com.mx>
2021-02-24 12:24:00 -0600

    [bugfix] procfile

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var app = express();
 const http = require('http').Server(app);
 
 app.use(express.static('public'));
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 var port_number = http.listen(process.env.PORT || 3000);
 app.listen(port_number);
 
